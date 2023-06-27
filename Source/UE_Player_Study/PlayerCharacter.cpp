e79e9eea9126d947cf13f7d0734f944a570c9bdb
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-06-27 23:36:36 +0900

    플레이어 돌진 추가

--- Source/UE_Player_Study/PlayerCharacter.cpp
+++ Source/UE_Player_Study/PlayerCharacter.cpp
@@ -1,12 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerCharacter.h"
+#include "TimerManager.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
+//생성자 함수
 APlayerCharacter::APlayerCharacter() 
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -26,8 +27,8 @@ APlayerCharacter::APlayerCharacter()
 
 	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 
-	//스프링 암의 클래스 변수를 설정합니다.
 
+	//스프링 암의 클래스 변수를 설정
 	SpringArmComp->bUsePawnControlRotation = true;
 
 	//캐릭터 무브먼트 컴포넌트의 클래스 변수를 설정합니다.
@@ -37,15 +38,30 @@ APlayerCharacter::APlayerCharacter()
 	GetCharacterMovement()->bUseControllerDesiredRotation = true;
 
 	GetCharacterMovement()->bIgnoreBaseRotation = true;
+
+    //점프 상태 확인 변수 초기화
+    IsJump = false;
+
+    //기본 돌진 속도 설정
+    DashSpeed = 3000.0f;
+
+    //돌진 지속 시간 설정
+    DashDuration = 3.0f;
+
+    //돌진 쿨타임 설정
+    DashCooldown = 20.0f;
+
+    //돌진 가능 확인 변수
+    IsDash = true;
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
+//앞, 뒤로 이동하는 함수
 void APlayerCharacter::MoveForward(float AxisValue)
 {
     if ((Controller != nullptr) && (AxisValue != 0.0f))
@@ -60,6 +76,7 @@ void APlayerCharacter::MoveForward(float AxisValue)
     }
 }
 
+//좌, 우로 이동하는 함수
 void APlayerCharacter::MoveRight(float AxisValue)
 {
     if ((Controller != nullptr) && (AxisValue != 0.0f))
@@ -76,23 +93,52 @@ void APlayerCharacter::MoveRight(float AxisValue)
     }
 }
 
+//달리기 시작 함수
 void APlayerCharacter::BeginSprint()
 {
     GetCharacterMovement()->MaxWalkSpeed = 1000.0f;
 }
 
+//달리기 종료 함수
 void APlayerCharacter::EndSprint()
 {
-    GetCharacterMovement()->MaxWalkSpeed = 600.f;
+    GetCharacterMovement()->MaxWalkSpeed = 600.0f;
 }
 
+void APlayerCharacter::DashInput()
+{
+    if (IsDash)
+    {
+        Dash();
 
+        GetWorldTimerManager().SetTimer(DashCooldownTimerHandle, this, &APlayerCharacter::EndDash, DashDuration, false);
+
+        IsDash = false;
+
+        GetWorldTimerManager().SetTimer(DashCooldownTimerHandle, this, &APlayerCharacter::ResetDashCooldown, DashCooldown, false);
+    }
+}
+
+void APlayerCharacter::Dash()
+{
+    GetCharacterMovement()->MaxWalkSpeed = DashSpeed; 
+}
+
+void APlayerCharacter::EndDash()
+{
+    GetCharacterMovement()->MaxWalkSpeed = GetClass()->GetDefaultObject<APlayerCharacter>()->GetCharacterMovement()->MaxWalkSpeed;
+}
+
+void APlayerCharacter::ResetDashCooldown()
+{
+    // 쿨타임이 끝났을 때 돌진 가능 상태로 설정
+    IsDash = true;
+}
 
 // Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -109,6 +155,7 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
     PlayerInputComponent->BindAction("Sprint", IE_Released, this, &APlayerCharacter::EndSprint);
     PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
     PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+    PlayerInputComponent->BindAction("Dash", IE_Pressed, this, &APlayerCharacter::DashInput);
 
 }
 
--- Source/UE_Player_Study/PlayerCharacter.h
+++ Source/UE_Player_Study/PlayerCharacter.h
@@ -10,7 +10,12 @@ UCLASS()
 class UE_PLAYER_STUDY_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
+private:
+	bool IsJump;
+	
+	//쿨타임 관리 변수
+	bool IsDash;
+	
 public:
 
 	// Sets default values for this character's properties
@@ -19,6 +24,19 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	//돌진 시 속도
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+		float DashSpeed;
+
+	//돌진 지속 시간
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+		float DashDuration;
+
+	//돌진 쿨타임
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+		float DashCooldown;
+
 
 	//스프링 암 컴포넌트가 플레이어 뒤에 있는 카메라를 따라 감
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
@@ -34,11 +52,26 @@ protected:
 	//좌, 우로 이동하는 함수
 	void MoveRight(float InputAxis);
 
-	//달리기 시작
+	//달리기 시작 함수
 	void BeginSprint();
 
-	//달리기 끝
+	//달리기 종료 함수
 	void EndSprint();
+
+	//돌진 입력 함수
+	void DashInput();
+
+	//돌진 실행 함수
+	void Dash();
+
+	//돌진 종료 함수
+	void EndDash();
+
+	//돌진 쿨타임 재설정 함수
+	void ResetDashCooldown();
+
+
+	FTimerHandle DashCooldownTimerHandle;
 
 public:	
 	// Called every frame
